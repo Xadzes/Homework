15e1a9ad3b2fa194222b895ec59cf3030745d80b
Xadzes <mafagerllion@gmail.com>
2025-06-05 21:59:15 +0300

    Удалил лишние файлы и папки из репозитория

--- src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { LoginPage } from "@pages/Login/Login";
-
-export default function Login() {
-  return (
-    <LoginPage title="Denzel" description="Washington"/>
-  );
-}
--- src/components/base/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './Button/Button';
-export { default as Icon } from './Icon/Icon';
-export { default as ImageWrapper } from './ImageWrapper/ImageWrapper';
